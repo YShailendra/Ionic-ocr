ad58692487bbf6459c05be62736dc4e63887f6ec
yshailendra <ershailendra57@gmail.com>
2018-12-25 15:39:40 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { Camera } from '@ionic-native/camera';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { NativeStorage } from '@ionic-native/native-storage';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ,
+    Camera,
+    TextToSpeech,
+    NativeStorage
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
